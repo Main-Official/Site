643229f8f6d531f5ea6e0f70fe3b64a495e5dc5b
Main-Official <69701658+Main-Official@users.noreply.github.com>
2020-10-15 05:54:17 +0200

    Add files via upload

---
+++ S/The dev cycle/index.php
@@ -0,0 +1,44 @@
+<?php include_once("header.php");?>
+<header>
+	<nav>
+		<h2><a href="#contact"><!-- Contact us --></a></h2>
+		<h1>The dev cycle</h1>
+		<h2 class="grey">Learn Software development at your own pace</h2>
+	</nav>
+</header>
+<section>
+	<div class="box">
+		<h3>Sign in</h3>
+		<form action="#">
+			<input type="text" placeholder="Username" name=""><br>
+			<input type="password" placeholder="Password" name=""><br>
+			<input type="submit" name="" value="Login">
+			<br>
+			<br>
+			or
+			<br>
+			<br>
+			<a href="registerform.php">Register</a>
+		</form>
+	</div>
+	<div>
+		<h3>About</h3>
+		<p> The dev cycle helps those who are interested in software development. Here you can discuss topics, have debates, find answeres to common questions and more.</p>
+	</div>
+	<div>
+		<h3 class="capital">beginners guide</h3>
+		<p>Most Projects will start with the "Hello World" example. You should have fun while you learn.</p>
+	</div>
+	<!-- ///// -->
+		<!-- <h3 id="contact">Contact us</h3>
+		<h3 class="contact">Cell - "number"</h3>
+		<h2 class="grey">Something Here</h2> -->
+		<!-- ///// -->
+		
+		<footer>
+			<p>&copy; The dev cycle</p>
+		</footer>
+	</section>
+
+</body>
+</html>
